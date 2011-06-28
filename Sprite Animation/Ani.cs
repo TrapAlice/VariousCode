2d2c472635932033e5551b8ebef929a0ccc605c0
TrapAlice <Goten44461@hotmail.com>
2011-06-28 23:53:06 +0100

    Initial commit for Sprite Animation and readme updated.

---
+++ Sprite Animation/Ani.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Animation
+{
+    class Ani
+    {
+        private Texture2D mSheet;
+        private int mWidth; private int mHeight;
+        private int mFrames;
+        private int mCurrentFrame;
+        private int mPos;
+        private float timer;
+        private float mInterval;
+        private float mScale;
+        private Vector2 mPosition;
+        private bool mLoop;
+
+        public Ani(Texture2D pSheet, int pPos, int pWidth, int pHeight, int pFrames, float pInterval)
+        {
+            mSheet = pSheet;
+            mWidth = pWidth;
+            mHeight = pHeight;
+            mFrames = pFrames;
+            mInterval = pInterval;
+            mPos = pPos;
+
+            mLoop = true;
+            mCurrentFrame = 0;
+            timer = 0;
+            mScale = 1;
+            mPosition = new Vector2(0, 0);
+            
+        }
+
+        public Ani(Texture2D pSheet, int pPos, int pWidth, int pHeight, int pFrames, float pInterval, bool pLoop)
+        {
+            mSheet = pSheet;
+            mWidth = pWidth;
+            mHeight = pHeight;
+            mFrames = pFrames;
+            mInterval = pInterval;
+            mPos = pPos;
+            mLoop = pLoop;
+
+            mCurrentFrame = 0;
+            timer = 0;
+            mScale = 1;
+            mPosition = new Vector2(0, 0);
+        }
+
+        public void draw(float pTime, bool pFlip)
+        {
+            Game1.spriteBatch.Begin();
+
+            Rectangle sprite = new Rectangle((mCurrentFrame) * mWidth, (mPos-1) * mHeight, mWidth, mHeight);
+            //Game1.spriteBatch.Draw(mSheet, mPosition, sprite, Color.White);
+            if(!pFlip)Game1.spriteBatch.Draw(mSheet, mPosition, sprite, Color.White, 0.0f, new Vector2(0, 0), mScale, SpriteEffects.None, 0.5f);
+            if (pFlip) Game1.spriteBatch.Draw(mSheet, mPosition, sprite, Color.White, 0.0f, new Vector2(0, 0), mScale, SpriteEffects.FlipHorizontally, 0.5f);
+
+            /*String timerString = "Timer: " +timer.ToString();
+            String CurrentFrameString = "Current Frame:" + mCurrentFrame.ToString();
+            Game1.spriteBatch.DrawString(Game1.font, timerString, new Vector2(100, 0), Color.Black);
+            Game1.spriteBatch.DrawString(Game1.font, CurrentFrameString, new Vector2(100, 20), Color.Black);
+            */
+            Game1.spriteBatch.End();
+
+            
+
+            timer += pTime;
+            if (timer > mInterval) { mCurrentFrame++; timer = 0; }
+            if (mCurrentFrame >= mFrames && mLoop) { mCurrentFrame = 0; }
+            else if (mCurrentFrame >= mFrames && !mLoop) { mCurrentFrame = mFrames-1; }
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+                return mPosition;
+            }
+            set
+            {
+                mPosition = value;
+            }
+        }
+
+        public float Scale
+        {
+            get
+            {
+                return mScale;
+            }
+            set
+            {
+                mScale = value;
+            }
+        }
+    }
+}
---
+++ Sprite Animation/Game1.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Animation
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        public static SpriteBatch spriteBatch;
+        public static SpriteFont font;
+        Ani test;
+        Ani test2;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Texture2D sprite = Content.Load<Texture2D>("sprite1");
+            test = new Ani(sprite, 2, 80, 60, 11, 0.5f, true);
+            test2 = new Ani(sprite, 3, 80, 60, 2, 2.0f, true);
+            //test.Scale = 5;
+            //test2.Scale = 5;
+            test2.Position = new Vector2(200, 0);
+
+            font = Content.Load<SpriteFont>("font");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            test2.draw(0.1f, false);
+            test.draw(0.1f,true);
+            
+            base.Draw(gameTime);
+        }
+    }
+}
