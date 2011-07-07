4d986c644efb8b6df6098ecf9955be52a46582b9
TrapAlice <Goten44461@hotmail.com>
2011-07-07 05:40:55 +0100

    Updated with the new version.

--- Sprite Animation/Ani.cs
+++ Sprite Animation/Ani.cs
@@ -1,109 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using Microsoft.Xna.Framework;
-
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 
 
 namespace Animation
 {
-    class Ani
-    {
-        private Texture2D mSheet;
-        private int mWidth; private int mHeight;
-        private int mFrames;
-        private int mCurrentFrame;
-        private int mPos;
-        private float timer;
-        private float mInterval;
-        private float mScale;
-        private Vector2 mPosition;
-        private bool mLoop;
+	class Ani
+	{
+		public enum Action
+		{
+			Idle=0,
+			Attack,
+			Guard,
+			Victory,
+			Hit,
+			Death,
+			Weak,
+		}
 
-        public Ani(Texture2D pSheet, int pPos, int pWidth, int pHeight, int pFrames, float pInterval)
-        {
-            mSheet = pSheet;
-            mWidth = pWidth;
-            mHeight = pHeight;
-            mFrames = pFrames;
-            mInterval = pInterval;
-            mPos = pPos;
+		private Texture2D mSheet;
+		private int mWidth;
+		private int mHeight;
+		private int mPos;
+		private float mTimer;
+		private float mInterval;
+		private bool mLoop;
+		private int mLoopFrom;
 
-            mLoop = true;
-            mCurrentFrame = 0;
-            timer = 0;
-            mScale = 1;
-            mPosition = new Vector2(0, 0);
-            
-        }
+		public int Frames { get; set; }
+		public int CurrentFrame { get; set; }
+		public float Scale { get; set; }
 
-        public Ani(Texture2D pSheet, int pPos, int pWidth, int pHeight, int pFrames, float pInterval, bool pLoop)
-        {
-            mSheet = pSheet;
-            mWidth = pWidth;
-            mHeight = pHeight;
-            mFrames = pFrames;
-            mInterval = pInterval;
-            mPos = pPos;
-            mLoop = pLoop;
 
-            mCurrentFrame = 0;
-            timer = 0;
-            mScale = 1;
-            mPosition = new Vector2(0, 0);
-        }
+		public Ani(Texture2D pSheet, int pPos, int pWidth, int pHeight, int pFrames, float pInterval)
+		{
+			mLoop = true;
+			CurrentFrame = 0;
+			mTimer = 0;
+			Scale = 1;
+			mLoopFrom = 0;
 
-        public void draw(float pTime, bool pFlip)
-        {
-            Game1.spriteBatch.Begin();
+			mSheet = pSheet;
+			mWidth = pWidth;
+			mHeight = pHeight;
+			Frames = pFrames;
+			mInterval = pInterval;
+			mPos = pPos;
+		}
 
-            Rectangle sprite = new Rectangle((mCurrentFrame) * mWidth, (mPos-1) * mHeight, mWidth, mHeight);
-            //Game1.spriteBatch.Draw(mSheet, mPosition, sprite, Color.White);
-            if(!pFlip)Game1.spriteBatch.Draw(mSheet, mPosition, sprite, Color.White, 0.0f, new Vector2(0, 0), mScale, SpriteEffects.None, 0.5f);
-            if (pFlip) Game1.spriteBatch.Draw(mSheet, mPosition, sprite, Color.White, 0.0f, new Vector2(0, 0), mScale, SpriteEffects.FlipHorizontally, 0.5f);
+		public Ani(Texture2D pSheet, int pPos, int pWidth, int pHeight, int pFrames, float pInterval, bool pLoop) :
+			this( pSheet, pPos, pWidth, pHeight, pFrames, pInterval )
+		{
+			mLoop = pLoop;
+		}
 
-            /*String timerString = "Timer: " +timer.ToString();
-            String CurrentFrameString = "Current Frame:" + mCurrentFrame.ToString();
-            Game1.spriteBatch.DrawString(Game1.font, timerString, new Vector2(100, 0), Color.Black);
-            Game1.spriteBatch.DrawString(Game1.font, CurrentFrameString, new Vector2(100, 20), Color.Black);
-            */
-            Game1.spriteBatch.End();
+		public Ani(Texture2D pSheet, int pPos, int pWidth, int pHeight, int pFrames, float pInterval, int pLoopFrom) :
+			this( pSheet, pPos, pWidth, pHeight, pFrames, pInterval )
+		{
+			mLoopFrom = pLoopFrom;
+		}
 
-            
+		public void draw(float pTime, Vector2 pPosition, bool pFlip)
+		{
+			Rectangle sprite = new Rectangle( ( CurrentFrame ) * mWidth, ( mPos - 1 ) * mHeight, mWidth, mHeight );
+			Vis.Draw( mSheet, pPosition, sprite, pFlip );
+			mTimer += pTime;
+			if ( mTimer >= mInterval ) { CurrentFrame++; mTimer = 0; }
+			if ( CurrentFrame >= Frames )
+			{
+				if ( mLoop ) { CurrentFrame = mLoopFrom; }
+				else { CurrentFrame = Frames - 1; }
+			}
+		}
 
-            timer += pTime;
-            if (timer > mInterval) { mCurrentFrame++; timer = 0; }
-            if (mCurrentFrame >= mFrames && mLoop) { mCurrentFrame = 0; }
-            else if (mCurrentFrame >= mFrames && !mLoop) { mCurrentFrame = mFrames-1; }
-        }
+		public bool Finished
+		{
+			get
+			{
+				if ( CurrentFrame >= Frames - 1 ) return true;
+				return false;
+			}
+		}
 
-        public Vector2 Position
-        {
-            get
-            {
-                return mPosition;
-            }
-            set
-            {
-                mPosition = value;
-            }
-        }
-
-        public float Scale
-        {
-            get
-            {
-                return mScale;
-            }
-            set
-            {
-                mScale = value;
-            }
-        }
-    }
+		public void reset()
+		{
+			CurrentFrame = 0;
+			mTimer = 0;
+		}
+	}
 }
